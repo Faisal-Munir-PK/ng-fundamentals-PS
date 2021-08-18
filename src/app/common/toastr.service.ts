b1f05511ba4b473715ab2e06fa34a1db631dc88e
Faisal_Munir <engineer.faisalmunir@gmail.com>
2021-08-18 12:16:57 +0100

    variable changed

--- src/app/common/toastr.service.ts
+++ src/app/common/toastr.service.ts
@@ -1,23 +1,20 @@
 import { Injectable } from "@angular/core"
-
-
-
+declare let toastr:any
 @Injectable()
 export class ToastrService{
-    private toastr:any;
     success(message:string, title?:string){
-        this.toastr.success(message, title)
+        toastr.success(message, title)
     }
 
     info(message:string, title?:string){
-        this.toastr.info(message, title)
+        toastr.info(message, title)
     }
 
     warning(message:string, title?:string){
-        this.toastr.warning(message, title)
+        toastr.warning(message, title)
     }
 
     error(message:string, title?:string){
-        this.toastr.error(message, title)
+        toastr.error(message, title)
     }
 }
